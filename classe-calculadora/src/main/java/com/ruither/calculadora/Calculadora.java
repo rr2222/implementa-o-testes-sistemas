7495cd1a7bda681ea2e4f25d645caaba5b77fae7
rr2222 <ruither.carvalho@gmail.com>
2021-09-08 14:10:02 -0300

    adicionado exercicio classe calculadora

---
+++ classe-calculadora/src/main/java/com/ruither/calculadora/Calculadora.java
@@ -0,0 +1,22 @@
+package com.ruither.calculadora;
+
+public class Calculadora {
+	public double somar(double num1, double num2){
+		return num1 + num2;
+	}
+
+	public double subtrair(double num1, double num2){
+		return num1 - num2;
+	}
+
+	public double multiplicar(double num1, double num2){
+		return num1*num2;
+	}
+
+	public double dividir (double num1, double num2){
+		return num1/num2;
+	}
+
+
+
+}
